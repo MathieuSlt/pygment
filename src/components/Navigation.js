1ef11a74bd7c6271cb20f48e4eba006fbf91ba83
SanjiKush <mathsa007@yahoo.fr>
2023-01-28 20:12:16 +0100

    form page + underline marche tjr pas

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,42 +1,41 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 import variables from '../styles/_settings.scss';
-import '../styles/components/_navigation.scss';
 
 
 const Navigation = () => {
 
-    const handleMouseOver = () => {
-        // console.log($colorUnderline);
-
-        const navigation = document.querySelector('.navigation')
-        console.log(navigation);
-        navigation.style.color = "green";
-        console.log(navigation.style);
-        // variables.color1 = "green";
+    const handleMouseOver = (color) => {
+        console.log(document.documentElement.style);
+        document.documentElement.style.setProperty('--colorUnderline', color);
     };
 
 
     const NavItem = (props) => {
         return <li
             style={{ color: props.color }}
-            onMouseOver={handleMouseOver}
+            onClick={handleMouseOver(props.color)}
         >{props.title}</li >;
     };
 
     return (
         <div className="navigation" id="repere">
             <ul>
-                <NavLink to="/news">
-                    <NavItem color={variables.newsColor} title="NEWS" />
+                <NavLink to="/news" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                    {/* <NavItem color={variables.newsColor} title="NEWS" /> */}
+                    <li
+                        style={{ color: variables.newsColor }}
+                        onClick={handleMouseOver(variables.newsColor)}>
+                        NEWS
+                    </li >
                 </NavLink>
-                <NavLink to="/artists">
+                <NavLink to="/artists" className={(nav) => (nav.isActive ? "nav-active" : "")}>
                     <NavItem color={variables.artistsColor} title="ARTISTS" />
                 </NavLink>
-                <NavLink to="/project">
+                <NavLink to="/project" className={(nav) => (nav.isActive ? "nav-active" : "")}>
                     <NavItem color={variables.projectColor} title="PROJECT" />
                 </NavLink>
-                <NavLink to="/login">
+                <NavLink to="/login" className={(nav) => (nav.isActive ? "nav-active" : "")}>
                     <NavItem color={variables.loginColor} title="LOGIN" />
                 </NavLink>
             </ul>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Navigation from '../components/Navigation';
+import LoginForm from '../components/LoginForm';
+import SignUpForm from '../components/SignUpForm';
 
 const Login = () => {
+
+
     return (
         <div>
             <Navigation />
+            {/* <LoginForm /> */}
+            <SignUpForm />
         </div>
     );
-};
+}
 
 export default Login;
+
+
